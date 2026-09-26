e4a2123a7820bfc02a2dc20877a7beec2bfc4fe8
surumeneco <128275841+surumeneco@users.noreply.github.com>
2026-09-26 23:05:41 +0900

    Restore launcher datapack copy import

--- Launcher.java
+++ Launcher.java
@@ -2,6 +2,7 @@ import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
